aa7c2cf7561c1f25e933c6f01f11b13ed943ad63
programeerik <erik.leusink22@gmail.com>
2023-09-12 11:51:45 +0200

    Added styling to the cv

---
+++ js/gdpr.js
@@ -0,0 +1,87 @@
+class GDPR {
+
+    constructor() {
+        this.showContent();
+        this.bindEvents();
+        if((this.cookieStatus() !== 'accept') || this.cookieStatus() !== 'reject') this.showGDPR();
+    }
+
+    bindEvents() {
+        let buttonAccept = document.querySelector('.gdpr-consent__button--accept');
+        buttonAccept.addEventListener('click', () => {
+            this.cookieStatus('accept');
+            this.showContent();
+            this.hideGDPR();
+        });
+
+        let buttonReject = document.querySelector('.gdpr-consent__button--reject');
+        buttonReject.addEventListener('click', () => {
+            this.cookieStatus('reject');
+            this.showContent();
+            this.hideGDPR();
+        });
+    }
+
+    showContent() {
+        this.resetContent();
+        const status = this.cookieStatus() == null ? 'not-chosen' : this.cookieStatus();
+        const element = document.querySelector(`.content-gdpr-${status}`);
+        element.classList.add('show');
+
+    }
+
+    resetContent(){
+        const classes = [
+            '.content-gdpr-accept',
+            '.content-gdpr-not-chosen',
+            '.content-gdpr-reject'];
+
+        for(const c of classes){
+            document.querySelector(c).classList.add('hide');
+            document.querySelector(c).classList.remove('show');
+        }
+    }
+
+
+
+    cookieStatus(status) {
+
+        if (status) localStorage.setItem('gdpr-consent-choice', status);
+
+//student uitwerking
+
+        return localStorage.getItem('gdpr-consent-choice');
+    }
+
+//student uitwerking
+
+    hideGDPR(){
+        document.querySelector(`.gdpr-consent`).classList.add('hide');
+        document.querySelector(`.gdpr-consent`).classList.remove('show');
+    }
+
+    showGDPR(){
+        document.querySelector(`.gdpr-consent`).classList.add('show');
+    }
+
+    // https://www.w3schools.com/js/js_cookies.asp
+    getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+}
+
+const gdpr = new GDPR();
+
